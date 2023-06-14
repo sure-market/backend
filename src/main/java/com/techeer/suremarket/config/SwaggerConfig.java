9a19251520b932b5f742c75459102050f184aba1
hyunwoo <ashyonu@gmail.com>
2023-06-14 15:06:51 +0900

    feat: swagger에 auth 추가

--- src/main/java/com/techeer/suremarket/config/SwaggerConfig.java
+++ src/main/java/com/techeer/suremarket/config/SwaggerConfig.java
@@ -6,8 +6,14 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -15,6 +21,8 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.OAS_30)
                 .useDefaultResponseMessages(false)
+                .securityContexts(List.of(this.securityContext())) // SecurityContext 설정
+                .securitySchemes(List.of(this.apiKey()))
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.techeer.suremarket.controller"))
                 .paths(PathSelectors.any())
@@ -28,5 +36,23 @@ public class SwaggerConfig {
                 .description("Team Joon - IS")
                 .version("1.0")
                 .build();
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return List.of(new SecurityReference("Authorization", authorizationScopes));
+    }
+
+    // ApiKey 정의
+    private ApiKey apiKey() {
+        return new ApiKey("Authorization", "Authorization", "header");
     }
 }
